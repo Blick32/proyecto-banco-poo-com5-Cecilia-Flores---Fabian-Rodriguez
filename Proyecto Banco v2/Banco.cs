abd538533599c8950515e405ebe16dc2455c692d
Blick32 <blickquack@gmail.com>
2025-11-03 13:53:11 -0300

    Banco.cs

--- Proyecto Banco v2/Banco.cs
+++ Proyecto Banco v2/Banco.cs
@@ -1,122 +1,178 @@
-﻿/*
- * Created by SharpDevelop.
- * User: blick
- * Date: 16/10/2025
- * Time: 09:42
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 using System;
 using System.Collections.Generic;
 
-namespace Proyecto_Banco_v2
+namespace tp_integrador_poo
 {
 	/// <summary>
 	/// Description of Banco.
 	/// </summary>
 	public class Banco
-{
-    //---------PROPIEDADES------------------
+	{
+		//---------PROPIEDADES------------------
     
-    private string nombre;
-    public string Nombre
-    {
-        get { return nombre; }
-        set { nombre = value; }
-    }
-    
-    
-
+	    private string nombre;
+	    
+	    public string Nombre
+	    {
+	        get { return nombre; }
+	        
+	        //agregar una exception
+	        set { 
+	        	nombre = value;
+	        }
+	    }
  
-    //-----------LISTAS-----------------------------
-    
-    private List<Cliente> clientes;
-    private List<Cuenta> cuentas;
-
+	    //-----------LISTAS-----------------------------
+	    
+	    private List<Cliente> clientes;
+	    private List<Cuenta> cuentas;
    
-
-
-   
-
-    //----------------CONSTRUCTORES-------------------------
+	    //----------------CONSTRUCTORES-------------------------
+	    
+	    public Banco(string nombre)
+	    {
+	    	//se accede a la propiedad para entrar a la validacion del tipo de dato
+	        Nombre = nombre;
+	        
+	        //se crean las listas en el constructor para que no de null
+	        clientes = new List<Cliente>();
+	        cuentas = new List<Cuenta>();
+	    }
+	    
+	    
+	     //-----------------MÉTODOS CLIENTES---------------------------------------
     
-    public Banco(string nombre)
-    {
-        this.nombre = nombre;
-        clientes = new List<Cliente>();
-        cuentas = new List<Cuenta>();
-    }
-
+	    public void AgregarCliente(Cliente c)
+	    {
+	        clientes.Add(c);
+	    }
+	
+	    public void EliminarCliente(Cliente c)
+	    {
+	        clientes.Remove(c);
+	    }
+	
+	    public Cliente BuscarCliente(string dni)
+	    {
+	    	StringNoVacio(dni);
+	    	if(!(dni.Length >= 7 && dni.Length <= 8)){
+    			throw new ArgumentOutOfRangeException("Ingrese entre 7 y 8 dígitos para el DNI. Por favor, ingrese un valor válido.");
+    		}
+	    	foreach(Cliente c in clientes){
+	    		if(c.Dni == dni){
+	    			//si el dni del objeto cliente es igual al dni ingresado
+	    			Console.WriteLine(c.Dni);
+	    			return c;
+	    		}else{
+	    			Console.WriteLine(c.Dni);
+	    		}
+	    	}
+	    	
+    		return null;
+    		//return BuscarClienteRecursivo(dni,0);
+	    }
+	    
+	    //metodo auxiliar
+	    /*
+	    private Cliente BuscarClienteRecursivo(string dni, int i){
+			if (i >= clientes.Count)
+				return null;
+			if (clientes[i].Dni == dni)
+				return clientes[i];
+			return BuscarClienteRecursivo(dni, i + 1);
+	    }
+	    */
+	
+	    public List<Cliente> ListarClientes()
+	    {
+	    	//aca hay que recorrer la lista y mostrar la informacion impresa en consola
+	        return clientes;
+	    }
+	
+	    public int CantidadClientes()
+	    {
+	        return clientes.Count;
+	    }
+	
+	    public void LimpiarClientes()
+	    {
+	        clientes.Clear();
+	    }
+	    
+	     //-------------------METODOS CUENTAS--------------------------
     
-   
-    
-    //-----------------MÉTODOS CLIENTES---------------------------------------
-    
-    public void AgregarCliente(Cliente c)
-    {
-        clientes.Add(c);
-    }
+	    public void AgregarCuenta(Cuenta c)
+	    {
+	        cuentas.Add(c);
+	    }
+	
+	    public void EliminarCuenta(Cuenta c)
+	    {
+	    	//buscar cuentas del cliente
+	        cuentas.Remove(c);
+	    }
+	
+	    public Cuenta BuscarCuenta(int nroCuenta)
+	    {
+	    	//recorrer con un foreach y buscar con un if
+	        //return cuentas.Find(x => x.NroCuenta == nroCuenta);
+	        
+	        //aca hay que recorrer la lista con un foreach
+	    	foreach(Cuenta c in cuentas){
+	    		if(c.NroCuenta == nroCuenta){
+	    			return c;
+	    		}
+	    	}
+    		return null;
+	    }
+	
+	    public List<Cuenta> ListarCuentas()
+	    {
+	        return cuentas;
+	    }
+	
+	    public int CantidadCuentas()
+	    {
+	        return cuentas.Count;
+	    }
+	
+	    public void LimpiarCuentas()
+	    {
+	        cuentas.Clear();
+	    }
 
-    public void EliminarCliente(Cliente c)
-    {
-        clientes.Remove(c);
-    }
+	
+	    
+	    //metodo para evitar string vacio
+	    private void StringNoVacio(string valor){
+	    	//validar vacio o nulo
+	    	if(valor == null || valor.Trim() == "" ){
+	    		throw new FormatException("Por favor ingrese un valor.");
+	    	}
+	    }
+	    
+	    
+	    //================== MÉTODOS RECURSIVOS ==================
 
-    public Cliente BuscarCliente(string dni)
-    {
-        return clientes.Find(x => x.Dni == dni);
-    }
+		public Cliente BuscarClienteRecursivo(string dni)
+	{
+    	if (string.IsNullOrWhiteSpace(dni))
+        return null;
 
-    public List<Cliente> ListarClientes()
-    {
-        return clientes;
-    }
+    	return BuscarClienteRec(dni, 0);
+	}
+	
 
-    public int CantidadClientes()
-    {
-        return clientes.Count;
-    }
+		private Cliente BuscarClienteRec(string dni, int indice)
+	{
+    	if (indice >= clientes.Count)
+        return null;
 
-    public void LimpiarClientes()
-    {
-        clientes.Clear();
-    }
+    	if (clientes[indice].Dni == dni)
+        return clientes[indice];
 
-    
-    
-    
-    
-    //-------------------METODOS CUENTAS--------------------------
-    
-    public void AgregarCuenta(Cuenta c)
-    {
-        cuentas.Add(c);
-    }
-
-    public void EliminarCuenta(Cuenta c)
-    {
-        cuentas.Remove(c);
-    }
-
-    public Cuenta BuscarCuenta(int nroCuenta)
-    {
-        return cuentas.Find(x => x.NroCuenta == nroCuenta);
-    }
-
-    public List<Cuenta> ListarCuentas()
-    {
-        return cuentas;
-    }
-
-    public int CantidadCuentas()
-    {
-        return cuentas.Count;
-    }
-
-    public void LimpiarCuentas()
-    {
-        cuentas.Clear();
-    }
+    	return BuscarClienteRec(dni, indice + 1);
 }
 
+	}
 }
